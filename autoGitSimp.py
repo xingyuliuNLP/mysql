ef7e8b79026b9a4877dbc43ea1f14b806f42a61d
xingyuliuNLP <xingyuliufrance@gmail.com>
2020-07-28 16:39:22 +0200

    restructure

--- autoGitSimp.py
+++ autoGitSimp.py
@@ -25,11 +25,11 @@ def on_modified(event):
 
 # create the event handler
 if __name__ == "__main__":
-    patterns = "*"
-    ignore_patterns = ""
-    ignore_directories = False
-    case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    # patterns = ""
+    # ignore_patterns = ""
+    # ignore_directories = False
+    # case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(ignore_patterns=["COMMIT_EDITMSG"])
 
     my_event_handler.on_created = on_created
     my_event_handler.on_deleted = on_deleted
